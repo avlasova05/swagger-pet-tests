b704c9c0ef82f7aa6bd212a75f5e4ed9f00c3b68
avlasova05 <avlasva@mail.ru>
2026-03-27 11:47:11 +0100

    new test for deleting

--- tests/API/store-tests.spec.ts
+++ tests/API/store-tests.spec.ts
@@ -45,3 +45,46 @@ test ('404 error when explicitly specified orderId @get', async() => {
     }
 }
 });
+
+test ('creating and deleting the purchase @delete', async () => {
+    const data_to_create = {
+  "petId": 1,
+  "quantity": 1,
+  "status": "placed",
+  "complete": true
+};
+    const response = await axios.post('https://petstore.swagger.io/v2/store/order', data_to_create, {
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        }
+    });
+    expect (response.status).toBe(200);
+    const CreatedId = BigInt(response.data.id);
+    console.log(CreatedId.toString());
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    try {
+        const getResponse = await axios.get(`https://petstore.swagger.io/v2/store/order/${CreatedId.toString()}`, {
+            headers: {
+                Accept: 'application/json'
+            }
+        });
+        console.log('exists:', getResponse.data);
+
+    const DeleteResponse = await axios.delete(`https://petstore.swagger.io/v2/store/order/${CreatedId.toString()}`, {
+        headers: {
+            Accept: 'application/json'
+        }
+    });
+    expect(DeleteResponse.status).toBe(200);
+    const VerifyResponse = await axios.get (`https://petstore.swagger.io/v2/store/order/${CreatedId.toString()}`, {
+        headers: { Accept: 'application/json'}
+    }).catch(error => error.response);
+    expect(VerifyResponse.status).toBe(404);
+}catch (error) {
+    console.log('no order found');
+}
+});
+ 
